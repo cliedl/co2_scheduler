82766e93e4bdccd4a7c0d6d0bb4c9e09563d57e4
cliedl <christian.liedl@gmx.net>
2024-02-21 15:06:11 +0100

    addeed src files

---
+++ src/co2_dictionary.py
@@ -0,0 +1,7 @@
+# in units of kW: https://www.daftlogic.com/information-appliance-power-consumption.htm
+co2_dictionary = {
+    "Washing Machine": 0.5,
+    "Dishwasher": 1.35,
+    "Oven": 2.15,
+    "A100 GPU": 0.5
+}
