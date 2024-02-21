d635b25a3ae4ded49d8741a5a3659d18b6c4a54d
matm01 <mumm.mathias@gmail.com>
2024-02-21 18:53:48 +0100

    fixed bug

--- src/plotting.py
+++ src/plotting.py
@@ -10,24 +10,22 @@ def get_tasks(df=None):
         return []
     else:
         tasks = df.copy()
-    for task in tasks.task_type:
-        # date_time = datetime.datetime.fromisoformat(tasks[tasks.task_type == task].datetime.iloc[0])
-        date_time = tasks[tasks.task_type == task].datetime.iloc[0]
-        for i in range(tasks[tasks.task_type == task].task_duration.iloc[0]):
-            tasks.loc[i + len(tasks)] = pd.NA
-            tasks.loc[:, 'task_type'].iloc[-1] = task
-            tasks.loc[:, 'datetime'].iloc[-1] = str(date_time + datetime.timedelta(hours=i + 1))
-            tasks.loc[:, 'optimal datetime'].iloc[-1] = str(date_time + datetime.timedelta(hours=i + 1))
+    task_list = []
+    for i in range(len(tasks)):
+        task_dict = {}
+        task_dict[tasks.task_type.iloc[i]] = list(tasks.iloc[i][['task_duration', 'datetime']])
+        task_list.append(task_dict)
 
-    tasks['datetime'] = pd.to_datetime(tasks['datetime'])
-    tasks['optimal datetime'] = pd.to_datetime(tasks['optimal datetime'])
+    for i in range(len(task_list)):
+        for key, value in task_list[i].items():
+            length = value[0]
+            date = value[1]
+            temp_list = []
+            for j in range(length):
+                temp_list.append(date + datetime.timedelta(hours=j))
+        task_list[i][key] = temp_list
 
-    tasks = tasks[['task_type', 'datetime', 'optimal datetime']]
-    tasks_tp = []
-    for i in tasks.task_type.unique():
-        tasks_tp.append((tasks[tasks.task_type == i]))
-
-    return tasks_tp
+    return task_list
 
 
 def plot_prediction(df, tasks):
@@ -41,6 +39,8 @@ def plot_prediction(df, tasks):
     traces = get_tasks(tasks)
     for trace in traces:
         fig.add_bar(
-            x=trace.datetime, y=df[df.time_axis.isin(trace.datetime)].co2_predictions, name=trace.task_type.iloc[0]
+            x=list(trace.values())[0],
+            y=df[df.time_axis.isin(list(trace.values())[0])].co2_predictions,
+            name=list(trace.keys())[0],
         ).update_layout(barmode='overlay')
     return fig
