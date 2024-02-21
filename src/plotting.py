282165cb8944ceecd039c5ddf48f9a18a44c6c9c
matm01 <mumm.mathias@gmail.com>
2024-02-21 14:01:00 +0100

    rework plot_function

--- src/plotting.py
+++ src/plotting.py
@@ -1,21 +1,46 @@
 import plotly.express as px
 import matplotlib.pyplot as plt
+import datetime
 import pandas as pd
 import numpy as np
+
+
+def get_tasks(df=None):
+    if df is None:
+        tasks = pd.read_csv('tasks.csv')
+    else:
+        tasks = df
+
+    for task in tasks.task_type:
+        date_time = datetime.datetime.fromisoformat(tasks[tasks.task_type == task].datetime.iloc[0])
+        for i in range(tasks[tasks.task_type == task].task_duration.iloc[0]):
+            tasks.loc[i + len(tasks)] = pd.NA
+            tasks.loc[:, 'task_type'].iloc[-1] = task
+            tasks.loc[:, 'datetime'].iloc[-1] = str(date_time + datetime.timedelta(hours=i + 1))
+            tasks.loc[:, 'optimal datetime'].iloc[-1] = str(date_time + datetime.timedelta(hours=i + 1))
+
+    tasks['datetime'] = pd.to_datetime(tasks['datetime'])
+    tasks['optimal datetime'] = pd.to_datetime(tasks['optimal datetime'])
+
+    tasks = tasks[['task_type', 'datetime', 'optimal datetime']]
+    tasks_tp = []
+    for i in tasks.task_type.unique():
+        tasks_tp.append((tasks[tasks.task_type == i]))
+
+    return tasks_tp
 
 
 def plot_prediction(df):
     x = df["time_axis"]
     y = df["co2_predictions"]
-    cmap = plt.get_cmap("RdYlGn_r")
-
-    y_scaled = (y-np.min(y))/(np.max(y)-np.min(y))
-
-    plotly_colormap = [
-        f'rgb{tuple(int(256 * x_) for x_ in cmap(y_)[:-1])}' for y_ in y_scaled]
-
-    fig = px.bar(x=x, y=y, color=y,
-                color_discrete_sequence=plotly_colormap)\
-                    .update_layout(showlegend=False)\
-                   
+    fig = px.bar(
+        x=x,
+        y=y,
+    )
+    traces = get_tasks()
+    for trace in traces:
+        fig.add_bar(
+            x=trace.datetime,
+            y=df[df.time_axis.isin(trace.datetime)].co2_predictions,
+        ).update_layout(barmode='overlay')
     return fig
